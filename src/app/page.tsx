da89f53f2b318c40bd3f2fa2a57e5baa07021fd5
ngorbach-d-one <nico.gorbach@d-one.ai>
2025-08-17 03:14:30 +0200

    misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-background text-wrap">
       <header className="w-full py-4 px-6 mb-8 flex fixed top-0 z-50 justify-center items-center bg-gradient-to-r from-purple-900 via-fuchsia-700 to-pink-600 text-white shadow-sm">
-        <div className="w-full max-w-5xl flex justify-between items-center">
+        <div className="w-full max-w-7xl flex justify-between items-center">
           <div className="flex items-center gap-2">
             <Image
               src="/bank-now-logo.svg"
